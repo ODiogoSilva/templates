008eacef9be72c7c8b9b8cc083bba307a2cac513
tiagofilipe12 <tiagofilipe12@gmail.com>
2018-04-13 17:22:34 +0100

    changed List_OF_FILES variable

--- pATLAS_consensus_json.py
+++ pATLAS_consensus_json.py
@@ -38,7 +38,8 @@ from assemblerflow_utils.assemblerflow_base import get_logger, MainWrapper
 logger = get_logger(__file__)
 
 if __file__.endswith(".command.sh"):
-    LIST_OF_FILES = ['$infile1', '$infile2']
+    # LIST_OF_FILES = ['$infile1', '$infile2']
+    LIST_OF_FILES = '$infile_list'.split()
     logger.debug("Running {} with parameters:".format(
         os.path.basename(__file__)))
     logger.debug("LIST_OF_FILES: {}".format(LIST_OF_FILES))
